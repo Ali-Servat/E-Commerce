a9b6b2bf4870c91b5e897e7da38a72cd2aaf0c47
Ali Servat <aliservat20@gmail.com>
2024-08-07 12:28:07 +0330

    remove modal-context file

--- src/App/Index.jsx
+++ src/App/Index.jsx
@@ -1,20 +1,17 @@
 import Router from "./Router";
 import BaseStyles from "./BaseStyles";
-import { ModalProvider } from "../Shared/context/modal-context";
 import { UserProvider } from "../Shared/context/user-context";
 import { CartProvider } from "../Shared/context/cart-context";
 
 function App() {
   return (
     <>
-      <ModalProvider>
-        <UserProvider>
-          <CartProvider>
-            <BaseStyles />
-            <Router />
-          </CartProvider>
-        </UserProvider>
-      </ModalProvider>
+      <UserProvider>
+        <CartProvider>
+          <BaseStyles />
+          <Router />
+        </CartProvider>
+      </UserProvider>
     </>
   );
 }
